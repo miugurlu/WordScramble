ed5368f9aa427c0074dda86865fe5c9a38d7874a
miugurlu <miugurlu@icloud.com>
2024-10-17 14:01:18 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by İbrahim Uğurlu on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
