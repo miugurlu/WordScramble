cd54345ffc62c44f35db54a73a11cc0cc16c1c7f
miugurlu <miugurlu@icloud.com>
2024-10-22 15:31:03 +0300

    showing derived words

---
+++ WordScramble/DerivedWordsView.swift
@@ -0,0 +1,18 @@
+//
+//  DerivedWordsView.swift
+//  WordScramble
+//
+//  Created by İbrahim Uğurlu on 22.10.2024.
+//
+
+import SwiftUI
+
+struct DerivedWordsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DerivedWordsView()
+}
